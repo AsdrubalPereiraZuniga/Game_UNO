574a3a81128196b4e0c4e6bef0ecb431f246dc70
JorgeRojas720s <jitorojas37@gmail.com>
2025-04-04 23:36:51 -0600

    add server and class flow

---
+++ src/main/java/server/Flow.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package server;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Enumeration;
+import players.Player;
+import server.Server;
+
+/**
+ *
+ * @author jorge
+ */
+public class Flow implements Runnable {
+
+    Socket socket;
+    DataInputStream readFlow;
+    DataOutputStream writeFlow;
+    String name;
+    //Mazo de cartas para cada jugador
+
+    public Flow(Socket socket, String name) {
+        this.socket = socket;
+        this.name = name;
+        try {
+            readFlow = new DataInputStream(
+                    new BufferedInputStream(socket.getInputStream()));
+            writeFlow = new DataOutputStream(
+                    new BufferedOutputStream(socket.getOutputStream()));
+        } catch (IOException ioe) {
+            System.out.println("IOException(Flujo): " + ioe);
+        }
+
+    }
+
+    @Override
+    public void run() {
+
+        Server.players.add(new Player(this, this.name));
+
+    }
+
+    public void broadcast(String message) {
+        synchronized (Server.players) {
+            Enumeration enumeration = Server.players.elements();
+            while (enumeration.hasMoreElements()) {
+                Player player = (Player) enumeration.nextElement();
+                Flow flow = (Flow) player.getFlow();
+                try {
+                    synchronized (flow.writeFlow) {
+                        flow.writeFlow.writeUTF(message);
+                        flow.writeFlow.flush();
+                    }
+                } catch (IOException ioe) {
+                    System.out.println("Error: " + ioe);
+                }
+            }
+        }
+    }
+
+}
---
+++ src/main/java/server/Server.java
@@ -0,0 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.Vector;
+import players.Player;
+import java.io.*;
+import java.net.*;
+
+
+/**
+ *
+ * @author jorge
+ */
+public class Server {
+
+    public static Vector<Player> players = new Vector();
+
+    public static void main(String args[]) {
+
+        ServerSocket serverSocket = initServer();
+
+        if (serverSocket != null) {
+            initConnection(serverSocket);
+        }
+
+    }
+
+    public static ServerSocket initServer() {
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(8000);
+            System.out.println("The server has stared...");
+            System.out.println("Waiting for players");
+            return serverSocket;
+        } catch (IOException ioe) {
+            System.out.println("Connection refused." + ioe);
+            System.exit(1);
+        }
+        return null;
+    }
+
+    public static void initConnection(ServerSocket serverSocket) {
+
+        while (true) {
+
+            try {
+                Socket socket = serverSocket.accept();
+                DataInputStream readFlow = new DataInputStream(
+                        new BufferedInputStream(socket.getInputStream()));
+                String name = readFlow.readUTF();
+                System.out.println("Connection accepted " + name);
+                
+                //Flujo flujo = new Flujo(nsfd, nombre, creaCarton());
+                
+                 //flujo.start();
+
+            } catch (Exception e) {
+                System.out.println("Error: " + e);
+            }
+
+        }
+
+    }
+
+}
