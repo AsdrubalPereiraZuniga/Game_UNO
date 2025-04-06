ef83f484d8bf2c419d5827edc607cc34c6798fc5
Yzma01 <marchena.isma@gmail.com>
2025-04-05 18:14:06 -0600

    commit for pull

--- src/main/java/com/mycompany/game_uno_so/PrimaryController.java
+++ src/main/java/com/mycompany/game_uno_so/PrimaryController.java
@@ -1,12 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
 package com.mycompany.game_uno_so;
 
-import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 
-public class PrimaryController {
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+/**
+ * FXML Controller class
+ *
+ * @author igmml
+ */
+public class PrimaryController implements Initializable {
+
 
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+    private AnchorPane bgView;
+    @FXML
+    private AnchorPane UsedCardsView;
+    @FXML
+    private AnchorPane DeckView;
+    @FXML
+    private AnchorPane Player0;
+    @FXML
+    private AnchorPane Player2;
+    @FXML
+    private AnchorPane Player1;
+    @FXML
+    private AnchorPane Player3;
+    @FXML
+    private Button oneBtn;
+    @FXML
+    private Button confirmBtn;
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+    @FXML
+    private void callOne(ActionEvent event) {
     }
+
+    @FXML
+    private void confirm(ActionEvent event) {
+    }
+
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.mycompany.game_uno_so {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens com.mycompany.game_uno_so to javafx.fxml;
     exports com.mycompany.game_uno_so;
