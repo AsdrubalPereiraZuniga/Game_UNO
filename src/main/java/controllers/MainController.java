d0ab034846f55d25d65d0c76e790ff7bd4a0e94c
Yzma01 <marchena.isma@gmail.com>
2025-04-26 12:18:40 -0600

    b

--- src/main/java/controllers/MainController.java
+++ src/main/java/controllers/MainController.java
@@ -85,7 +85,6 @@ public class MainController implements Initializable {
         setOtherPlayers();
         instance.lastCard = null;
         setTopCard(instance.client.getTopCard());
-
     }
 
     private void setOtherPlayers() {
